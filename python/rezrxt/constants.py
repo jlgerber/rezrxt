6218b0d9ded7f1492ccecdb6698093a9bded826a
Jonathan Gerber <jlgerber@gmail.com>
2017-08-25 07:56:46 -0700

    adding new command

--- python/rezrxt/constants.py
+++ python/rezrxt/constants.py
@@ -1,7 +1,11 @@
 """
 constants.py - constants
 """
-
+# root of the database
 REZRXT_DB_ROOT = "REZRXT_DB_ROOT"
+
 # env var name for context
 REZRXT_CTX = "REZRXT_CTX"
+
+# directory where we are going to store wrappers
+REZRXT_WRAPPER_DIR = "REZRXT_WRAPPER_DIR"
